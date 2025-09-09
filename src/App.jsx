a6cae9821d6b8d24293341346c768c2458ec9966
apple <himesh.kashyap@celestialitverse.com>
2025-09-09 21:41:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -112,6 +112,7 @@ function App() {
                 </AdminProtectedRoute>
               }
             />
+            
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
